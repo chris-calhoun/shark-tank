cb3e3869a7434b6d4855683f2ceff691cb928dc5
Chris Calhoun <[ctcalhoun2@gmail.com]>
2020-11-09 21:24:36 -0600

    show shark tank component.

---
+++ src/components/sharkTank/index.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import getStudents from '../../helpers/data/studentData';
+
+export default class sharkTank extends Component {
+  state = {
+    students: [],
+  };
+
+  componentDidMount() {
+    getStudents().then((response) => {
+      this.setState({
+        students: response,
+      });
+    });
+  }
+
+  render() {
+    const { students } = this.state;
+    console.warn({ students });
+    return <div className='shark-tank'></div>;
+  }
+}
--- src/helpers/data/studentData.js
+++ src/helpers/data/studentData.js
@@ -2,13 +2,12 @@ import Axios from 'axios';
 
 const baseUrl = 'https://shark-attack-60f42.firebaseio.com/students.json';
 
-const getStudents = () =>
-  new Promise((resolve, reject) => {
-    Axios.get(`${baseUrl}`)
-      .then((response) => {
-        resolve(response.data);
-      })
-      .catch((error) => reject(error));
-  });
+const getStudents = () => new Promise((resolve, reject) => {
+  Axios.get(`${baseUrl}`)
+    .then((response) => {
+      resolve(response.data);
+    })
+    .catch((error) => reject(error));
+});
 
 export default getStudents;
