247130a45907ce55e6754df2e7fd68809f0937d2
Chris Calhoun <[ctcalhoun2@gmail.com]>
2020-11-17 08:03:50 -0600

    console logged unlucky student.

--- src/App/index.js
+++ src/App/index.js
@@ -1,28 +1,45 @@
 import React from 'react';
 import './App.scss';
 import SharkTank from '../components/sharkTank';
-import { livingStudents } from '../helpers/data/studentData';
+import { livingStudents, dearlyBeloved } from '../helpers/data/studentData';
 
 class App extends React.Component {
   state = {
     liveStudents: [],
+    deadStudents: [],
   };
 
   componentDidMount() {
-    livingStudents().then((resp) => {
-      this.setState({
-        liveStudents: resp,
-      });
+    this.loadLivingStudents();
+    this.loadDeadStudents();
+  }
+
+  loadLivingStudents = () => livingStudents().then((resp) => {
+    this.setState({
+      liveStudents: resp,
     });
+  });
+
+  loadDeadStudents = () => dearlyBeloved().then((resp) => {
+    this.setState({
+      deadStudents: resp,
+    });
+  });
+
+  sharkAttack = () => {
+    if (this.state.liveStudents.length) {
+      const unluckyStudent = this.state.liveStudents[Math.floor(Math.random() * this.state.liveStudents.length)];
+      console.warn(unluckyStudent);
+    }
   }
 
   render() {
-    const { liveStudents } = this.state;
+    const { liveStudents, deadStudents } = this.state;
     return (
       <div className='App'>
         <nav className='navbar navbar-light navbar-center bg-dark'>
           <form className='form-inline'>
-            <button className='btn btn-danger' type='button'>
+            <button className='btn btn-danger' onClick={this.sharkAttack} type='button'>
               Shark Attack
             </button>
           </form>
